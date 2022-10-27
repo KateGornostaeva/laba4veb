91d91d7700853aa18309ceeb07250eb40f4b0c6a
Gornostaeva <kate.gornostaeva@inbox.ru>
2022-10-27 22:26:36 +0300

    change feedback

--- feedback.php
+++ feedback.php
@@ -6,21 +6,21 @@
         <p><input type="text" id = "fio" size="40"></p>
 
         <p>Электронная почта</p>
-        <p><input type="text" id = "email" size="40"></p>
+        <p><input type="text" id = "email" placeholder="email" size="40"></p>
 
-        <p>Откуда вы о нас узнали?</p>
-        <p><input type="radio" name="answer" value="a1">Учеба<Br>
-        <input type="radio" name="answer" value="a2">Работа<Br>
-        <input type="radio" name="answer" value="a3">Друзья</p>
+        <p>Введите ваше сообщение</p>
+        <p><textarea name="comment" cols="40" rows="6"></textarea></p></p>
+        <p><input type="file" name="f">
 
         <p><select name="select">
             <option>Жалоба</option>
             <option>Предложение</option>
         </select></p>
-
-        <p>Введите ваше сообщение</p>
-        <p><textarea name="comment" cols="40" rows="6"></textarea></p></p>
-        <p><input type="file" name="f">
+        
+        <p>Откуда вы о нас узнали?</p>
+        <p><input type="radio" name="answer" value="a1">Реклама из интернета<Br>
+        <input type="radio" name="answer" value="a2">Рассказали друзья<Br>
+    
 
         <p><input type="checkbox" name="a" value="согласие"> Даю согласие на обработку персональных данных</p>
         

1e310351f981c8f1d429579e2badcc808f0cd49b
Gornostaeva <kate.gornostaeva@inbox.ru>
2022-11-17 23:05:55 +0300

    start laba 5

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer id ="footer">
-    <p>Контактная информация</p>
-    <p>Горностаева Екатерина 211-362</p>
-    <p>Почта: kate.gornostaeva@inbox.ru </p>
-    <p><a href = "<?php $name='Обратная связь'; $link ='site_feedback.php'; $current_page=true; echo $link;?>">
-        <?php if( $current_page ) echo $name;?> <!--второе включение PHP‐кода -->
-    </a></p>
-    <p><?php echo 'Сформировано ', $today, " в ", $time?></p>
-</footer>
